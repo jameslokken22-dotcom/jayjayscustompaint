4e68279521b753d7feb6a9020d34f7f0ca964a11
jameslokken22-dotcom <jameslokken22@gmail.com>
2026-05-18 03:47:48 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,4 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.v}
+    <html lang="en" className={`${geistSans.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
